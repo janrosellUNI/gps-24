ab67ac2477b3eda77be835a704fca41362b37324
janrosellUNI <jan.rosell.caba@estudiantat.upc.edu>
2024-12-20 10:53:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,17 +2,30 @@ import unittest
 import transform
 
 
+
 class TestStringMethods(unittest.TestCase):
+    """
+        Esta clase contiene las pruebas unitarias para la función 'StringMethods'.
+        """
 
     def test_is_upper(self):
+        """
+                Esta función cambia un string a mayusculas.
+                """
         sting = transform.to_upper_case("hello")
         self.assertEqual(sting, "HELLO")
 
     def test_is_lower(self):
+        """
+                Esta función cambia un string a minusculas.
+                """
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
 
     def test_is_capitalize(self):
+        """
+                Esta función capitaliza un string.
+                """
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
 
